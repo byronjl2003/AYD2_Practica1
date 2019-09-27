3b916d0318061a9e597c440ce5af414c8006d6d1
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2019-09-26 20:46:49 -0600

    Arreglos en index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const express = require('express');
+var ip = require('ip');
 
 // Constants
 const PORT = 8080;
-const HOST = 'localhost';
+const HOST = ip.address();
 
 // App
 const app = express();
@@ -12,5 +13,6 @@ app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
-app.listen(PORT, HOST);
+app.listen(PORT,HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+console.log(ip.address());
